39e04e3ec974fc1194d0a094c9e929ab11259cff
tekdoken <tekdokenzx@gmail.com>
2021-12-14 20:59:44 +0700

    bai tap 1

---
+++ src/main/java/bai1_Sandwich/sandwichController.java
@@ -0,0 +1,14 @@
+package bai1_Sandwich;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class sandwichController {
+
+//    @RequestMapping("/save")
+//    public String save(@RequestParam("condiment") String[] condiment) {
+//
+//    }
+}
